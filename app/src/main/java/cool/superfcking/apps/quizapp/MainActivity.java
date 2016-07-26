a1cc0965af8d15f4c3f0b400b16ce89fcbcb97d7
Jeff Sinclair <jsinclair@atlassian.com>
2016-07-26 01:00:28 -0700

    second commit to switch to using Toast for results

--- app/src/main/java/cool/superfcking/apps/quizapp/MainActivity.java
+++ app/src/main/java/cool/superfcking/apps/quizapp/MainActivity.java
@@ -1,5 +1,6 @@
 package cool.superfcking.apps.quizapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -99,7 +101,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayMessage(String message){
-        TextView resultTextView = (TextView) findViewById(R.id.result_text_view);
-        resultTextView.setText(message);
+        Context context = getApplicationContext();
+
+        CharSequence text = message;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
